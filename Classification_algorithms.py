42ae4b9c64fd0603581a1ed758635de40cd4b5c7
AI Team <91843764+Iznachaliy@users.noreply.github.com>
2023-02-10 22:21:18 +0300

    Create Classification_algorithms.py

---
+++ Classification_algorithms.py
@@ -0,0 +1,6 @@
+classification_algorithms = ["Logistic regression", "Multinomial logistic regression", "Naive Bayes","Multinomial Naive Bayes",
+                             "Support Vector Machines", "Multiclass Support Vector Machines", "Decision tree", "Random Forest",
+                             "K-Nearest Neighbors", "Artificial Neural Networks", "XGBoost Classifier", "CatBoost Classifier",
+                             "LightGBM Classifier"]
+
+print(len(classification_algorithms))
