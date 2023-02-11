640ad4de134d0dba5cf37a4421539010fef48677
AI Team <91843764+Iznachaliy@users.noreply.github.com>
2023-02-11 12:50:31 +0300

    Create Dimensionality_reduction_techniques.py

---
+++ Dimensionality_reduction_techniques.py
@@ -0,0 +1,7 @@
+Dimensionality_reduction_techniques = ["PCA", "ICA", "FastICA", "FA", "EFA", "CFA", "t-SNE", "UMAP",
+                                       "LDA", "SVD", "kPCA", "NMF", "ISOMAP", "DCA", "CDCA", "CCA",
+                                       "KCCA", "PGD", "GDA", "LLE", "SM", "Hessian LLE", "GrandPrix",
+                                       "GPLVM", "ZIFA", "Laplacian Eigenmaps", "PPCA", "Diffusion map",
+                                       "SIMLR", "scvis"]
+
+print(len(Dimensionality_reduction_techniques))
