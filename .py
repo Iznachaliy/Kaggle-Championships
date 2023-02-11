41d0a6152c57628179ef51e6218a786145ef4862
AI Team <91843764+Iznachaliy@users.noreply.github.com>
2023-02-11 13:34:10 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,14 @@
+Tools = ["SQL", "Cassandra", "Hive", "Presto", "Kafka", "Hugging Face", "Hadoop",
+         "Apache Airflow", "Hugging Face Transformers", "MLFlow", "ClearML", "Faiss",
+         "BigARTM", "BERTopic", "ONNX", "TensorRT", "Git", "Bash", "Docker", "Docker Compose",
+         "Linux", "R-CNN", "Fast R-CNN", "Faster R-CNN", "Mask R-CNN", "YOLOv5", "Flask",
+         "Django", "PySpark", "PyTorch", "TensorFlow", "CatBoost", "XGBoost", "LightGBM",
+         "CI/CD", "scikit-learn", "NumPy", "pandas", "Keras", "matplotlib", "SciPy",
+         "Seaborn", "ClickHouse", "Celery", "Dagster", "Faust", "PostgreSQL", "Redis", "Ignite"
+         "Rabbit", "Gitlab-ci", "Kubernetes", "MySQL", "Hazelcast", "Aerospike", "MongoDB",
+         "Apache Superset", "Apache HBase", "Luigi", "HDFS", "Apache Phoenix", "Grafana",
+         "H20.ai", "Power BI", "Tableau", "R Shiny", "Plotly", "Amazon Athena" "PrestoDB",
+         "Trino", "NLTK", "PyMorphy2", "PyMystem3", "PyScript", "KubeFlow", "CUDA", "VAE",
+         "OpenCV", "TeamCity", "Jenkins", "Bamboo", "OpenShift", "Prometheus" "Nifi",
+         "JupyterHub", "InfluxDB", "Elasticsearch", "Cassandra", "fastText", "MapReduce"]
+         
