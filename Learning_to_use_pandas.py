994bd3b9e31ee88d56610ac35a04c530001c09a8
AI Team <91843764+Iznachaliy@users.noreply.github.com>
2023-02-13 22:05:45 +0300

    Create Learning_to_use_pandas.py

---
+++ Learning_to_use_pandas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+d = {
+     'Machine Learning Tasks': ['Clustering', 'Classification', 'Regression Analysis', 'Dimensionality Reduction'],
+     'Example of algorithms': ['K-means', 'SVM', 'Linear Regression', 'PCA']}
+df = pd.DataFrame(data=d)
+df
